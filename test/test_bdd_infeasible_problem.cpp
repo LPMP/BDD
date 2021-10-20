47c0ddb8a6457f4f313eb38867223dcb3f1a58d5
pawelswoboda <pswoboda@mpi-inf.mpg.de>
2021-10-20 10:52:52 +0200

    Added infeasible problem test

---
+++ test/test_bdd_infeasible_problem.cpp
@@ -0,0 +1,34 @@
+#include "ILP_parser.h"
+#include "bdd_preprocessor.h"
+#include "bdd_parallel_mma.h"
+#include "bdd_mma_vec.h"
+#include "test.h"
+
+using namespace LPMP;
+
+const char * infeasible_problem = 
+R"(Minimize
+-2 x_11 - 1 x_12 - 1 x_13
+-1 x_21 - 2 x_22 - 1 x_23 + 3 x_24
+-1 x_31 - 1 x_32 - 2 x_33 - 3.5 x_24
+Subject To
+x_11 + x_12 + x_13 = 1 
+x_21 + x_22 + x_23 + x_24 = 2 
+x_31 + x_32 + x_33 + x_24 = 1 
+x_11 + x_21 + x_31 = 1 
+x_12 + x_22 + x_32 = 1 
+x_13 + x_23 + x_33 = 1 
+End)";
+
+int main(int argc, char** argv)
+{
+    const ILP_input ilp = ILP_parser::parse_string(infeasible_problem);
+    bdd_preprocessor pre(ilp);
+
+    {
+        bdd_parallel_mma<float> solver(pre.get_bdd_collection(), ilp.objective().begin(), ilp.objective().end());
+        for(size_t i=0; i<10; ++i)
+            solver.iteration();
+        test(solver.lower_bound() == std::numeric_limits<double>::infinity());
+    }
+} 
