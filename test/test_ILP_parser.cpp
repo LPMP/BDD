4a31e51f8a85b3e772d84aaff1aa20940f70d45c
Ahmed Abbas <ahmed.a.mirza@hotmail.com>
2022-01-19 15:36:55 +0100

    additional characters for ILP parser

--- test/test_ILP_parser.cpp
+++ test/test_ILP_parser.cpp
@@ -15,6 +15,16 @@ Bounds
  x2 >= 0
 End)";
 
+const std::string ILP_example_hash =
+R"(Minimize
+x1 + 2*x2 + 1.5 * x#3 - 0.5*x#4 - x3
+Subject To
+ b_cuta_;0;14_1;@41d: x1 + 2*x2 + 3 * x#3 - 5*x#4 - x3 >= 1
+Bounds
+ x1 <= 1
+ x2 >= 0
+End)";
+
 const std::string nonlinear_example =
 R"(Minimize
 x1 + 2*x2 + 1.5 * x3 - 0.5*x4 - x5
@@ -40,6 +50,27 @@ int main(int argc, char** argv)
     test(input.objective("x3") == 1.5);
     test(input.objective("x4") == -0.5);
     test(input.objective("x5") == -1.0);
+
+    input.write_lp(std::cout);
+    test(input.nr_constraints() == 1);
+    }
+
+    // ILP_example_hash
+    {
+    const ILP_input input = ILP_parser::parse_string(ILP_example_hash);
+
+    test(input.nr_variables() == 5);
+    test(input.var_exists("x1"));
+    test(input.var_exists("x2"));
+    test(input.var_exists("x#3"));
+    test(input.var_exists("x#4"));
+    test(input.var_exists("x3"));
+
+    test(input.objective("x1") == 1.0);
+    test(input.objective("x2") == 2.0);
+    test(input.objective("x#3") == 1.5);
+    test(input.objective("x#4") == -0.5);
+    test(input.objective("x3") == -1.0);
 
     input.write_lp(std::cout);
     test(input.nr_constraints() == 1);
